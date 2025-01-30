fc2e1146a1aad08617809e8efc82d33dd25ef6df
Jason Pickering <jason.p.pickering@gmail.com>
2025-01-30 11:21:33 +0100

    Align to DHIS2 server names

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from prometheus_client import start_http_server, Metric, REGISTRY
 import json
 import sys
@@ -28,37 +30,71 @@ class JsonCollector(object):
 
     @staticmethod
     def collect_object_counts(response):
-        metric = Metric('dhis_object_counts', 'Object counts', 'gauge')
+        metric = Metric('dhis_summary_object_count_total', 'Object counts', 'gauge')
         for k, v in response['objectCounts'].items():
-            metric.add_sample('dhis_object_counts', value=v, labels={'object': k})
+            metric.add_sample('dhis_summary_object_count_total', value=v, labels={'object': k})
         return metric
 
     @staticmethod
     def collect_active_users(response):
-        metric = Metric('dhis_active_users', 'Active users', 'gauge')
+        metric = Metric('dhis_summary_active_users_count_total', 'Active users', 'gauge')
         for k, v in response['activeUsers'].items():
-            metric.add_sample('activeUsers', value=v, labels={'days': k})
+            metric.add_sample('dhis_summary_active_users_count_total', value=v, labels={'days': k})
         return metric
 
     @staticmethod
     def collect_data_values_count(response):
-        metric = Metric('dhis_datavalues_count', 'Count of data values', 'gauge')
+        metric = Metric('dhis_summary_data_value_count_total', 'Count of data values', 'gauge')
         for k, v in response['dataValueCount'].items():
-            metric.add_sample('dataValueCount', value=v, labels={'days': k})
+            metric.add_sample('dhis_summary_data_value_count', value=v, labels={'days': k})
         return metric
 
     @staticmethod
     def collect_event_count(response):
-        metric = Metric('dhis_event_count', 'Count of events', 'gauge')
+        metric = Metric('dhis_summary_event_count_total', 'Count of events', 'gauge')
         for k, v in response['eventCount'].items():
-            metric.add_sample('eventCount', value=v, labels={'days': k})
+            metric.add_sample('dhis_summary_event_count', value=v, labels={'days': k})
         return metric
 
     @staticmethod
-    def transform_summaries_to_metrics(summaries):
-        metric = Metric('dhis_metadata_integrity_checks', 'Metadata integrity checks', 'gauge')
+    def collect_build_info(response):
+        metric = Metric('dhis_summary_build_info', 'Build information', 'gauge')
+        #Convert the build time to seconds since the epoch
+
+        try:
+            build_time = int(time.mktime(time.strptime(response['system']['buildTime'], "%Y-%m-%dT%H:%M:%S.%f")))
+        except ValueError:
+            print("Error parsing build time")
+            build_time = 0
+
+        metric.add_sample('dhis_summary_build_info', value=build_time, labels={'version': response['system']['version'], 'commit': response['system']['revision']})
+        return metric
+
+    @staticmethod
+    def transform_count_to_metrics(summaries):
+        count_metric = Metric('dhis_data_integrity_issues_count_total', 'Data integrity check counts', 'gauge')
         for k, v in summaries.items():
-            metric.add_sample('dhis_metadata_integrity_checks', value=v['count'], labels={'count': k})
+            count_metric.add_sample('dhis_data_integrity_issues_count_total', value=v['count'], labels={'check': k, 'severity': v['severity'], 'object_type': v['issuesIdType']})
+        return count_metric
+
+    @staticmethod
+    def transform_percentage_to_metrics(summaries):
+        metric = Metric('dhis_data_integrity_issues_percentage', 'Data integrity check percentages', 'gauge')
+        for k, v in summaries.items():
+                #percentage can be missing entirely, so check for this
+            if 'percentage' in v:
+                metric.add_sample('dhis_data_integrity_issues_percentage', value=v['percentage'], labels={'check': k, 'severity': v['severity'], 'object_type': v['issuesIdType']} )
+        return metric
+
+    @staticmethod
+    def transform_duration_to_metrics(summaries):
+        metric = Metric('dhis_data_integrity_issues_duration_total', 'Data integrity check durations', 'gauge')
+        for k, v in summaries.items():
+            start_time = datetime.fromisoformat(v['startTime'])
+            end_time = datetime.fromisoformat(v['finishedTime'])
+            duration = (end_time - start_time).total_seconds()
+            metric.add_sample('dhis_data_integrity_issues_duration_total', value=duration,
+                               labels={'check': k, 'severity': v['severity'], 'object_type': v['issuesIdType']})
         return metric
 
     def fetch_metadata_integrity_checks(self):
@@ -67,8 +103,7 @@ class JsonCollector(object):
             url = self._config['server']['base_url'] + 'api/dataIntegrity/summary'
             response = self.http.request("GET", url, headers=self.request_headers)
             summaries =  json.loads(response.data.decode("utf-8"))
-            transformed_metrics = self.transform_summaries_to_metrics(summaries)
-            return transformed_metrics
+            return summaries
         except Exception as e:
             print("Error: " + str(e))
             return None
@@ -80,7 +115,12 @@ class JsonCollector(object):
         yield self.collect_active_users(response)
         yield self.collect_data_values_count(response)
         yield self.collect_event_count(response)
-        yield self.fetch_metadata_integrity_checks()
+        yield self.collect_build_info(response)
+        di_checks = self.fetch_metadata_integrity_checks()
+        if di_checks:
+            yield self.transform_count_to_metrics(di_checks)
+            yield self.transform_percentage_to_metrics(di_checks)
+            yield self.transform_duration_to_metrics(di_checks)
 
 if __name__ == '__main__':
 
